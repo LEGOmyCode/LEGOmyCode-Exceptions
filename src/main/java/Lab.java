9dfb3dfd96d16e2547ff4febd2f430d32404cc0f
RAM <mojicaxr@gmail.com>
2023-03-03 00:01:08 +0000

    Changed String to Null

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -21,7 +21,7 @@ public class Lab {
      * Check out this guide on throwing exceptions: https://rollbar.com/guides/java/how-to-throw-exceptions-in-java/#
      */
     public void mustThrow()throws Exception{
-        String test = "Throw Exception";
+        String test = null;
         test.toString();
     }
 }
